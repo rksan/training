831cad64b03ac2c3dad0d30b432633e347039c07
rk <69895506+rksan-dev@users.noreply.github.com>
2021-03-04 14:46:56 +0900

    version 1.1.0 released.

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# training
+# Training
 
 ## Typing game
 
-[Link](https://rksan.github.io/training/typinggame)
+[Link](https://rksan.github.io/training/typinggame/v1.1.0)
+
+#### history
+- version 1.1.0
+    + 修正
+        * ver 1.0.0-1 を修正
+- version 1.0.0
+    + 初版
+    + 既知のバグ
+        * ver 1.0.0-1 - 例に数字を入力できるにも関わらず、タイピングでは数字は判定外としている問題。
+    + 非推奨コード
+        * × [KeybordEvent.keyCode](https://developer.mozilla.org/ja/docs/Web/API/KeyboardEvent/keyCode) -> ○ [KeybordEvent.code](https://developer.mozilla.org/ja/docs/Web/API/KeyboardEvent/code)
+
---
+++ typinggame/v1.1.0/app.js
@@ -0,0 +1,98 @@
+(function () {
+
+    var QUESTION = localStorage.QUESTION || 'schoo WEB campus';
+    var SPACE = '&nbsp;'; //add @rk
+
+    var types = [];
+    var timer = null;
+    var startTime = 0;
+
+    function init() {
+        document.querySelector('.container').innerHTML = '';
+
+        types = QUESTION.split('').map(function (str) {
+            var type = document.createElement('span');
+            type.className = 'type';
+
+            //add start @rk
+            if (str === ' ') {
+                type.innerHTML = SPACE;
+            } else {
+                type.textContent = str;
+            }
+
+            //Remember the original character as the 'data-original-text' attribute as it has been converted to html
+            type.setAttribute('data-original-text', str);
+            //add end @rk
+
+            document.querySelector('.container').appendChild(type);
+
+            return type;
+        });
+
+        timerEnd();
+
+        document.querySelector('.timer').textContent = '0.000';
+    };
+
+    init();
+
+    function timerStart() {
+        startTime = new Date().getTime();
+        timer = setInterval(function () {
+            var time = (new Date().getTime() - startTime) / 1000;
+            document.querySelector('.timer').textContent = time.toFixed(3);
+        }, 10);
+    }
+
+    function timerEnd() {
+        clearInterval(timer);
+        timer = null;
+    }
+
+    document.addEventListener('keydown', function (event) {
+        var keyCode = event.code || String(event.which || event.keyCode);
+        var key = '';
+
+        if (keyCode === 13) { //enter key
+            init();
+            return;
+        }else if (keyCode === 32) { //space key
+            key = ' ';
+        }else{
+            key = event.key || String.fromCharCode(keyCode);;
+        }
+
+        if (key) {
+
+            if (timer === null) {
+                timerStart();
+            }
+
+            var next = types[0];
+            var originalText = next.getAttribute('data-original-text'); //add @rk
+
+            if (originalText === key) { //change @rk
+                next.classList.add('ok');
+                types.shift();
+                if (types.length === 0) {
+                    timerEnd();
+                }
+            } else {
+                next.classList.add('ng');
+            }
+        }
+
+        console.log(key);
+    });
+
+    document.querySelector('.container').addEventListener('click', function (event) {
+        var text = prompt('問題文を入力して下さい', QUESTION);
+        if (text) {
+            QUESTION = text;
+            localStorage.QUESTION = text;
+            init();
+        }
+    });
+
+})();
